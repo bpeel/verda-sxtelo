5c6235ce628d85477da7c771d6c9222125cb5a8e
Neil Roberts <bpeeluk@yahoo.co.uk>
2011-05-28 19:35:15 +0100

    Add an object to represent a person
    This adds GmlPerson which is a GObject subclass representing a
    person. People all have an ID which is a 64-bit number that is
    randomly generated.

---
+++ src/gml-person.c
@@ -0,0 +1,104 @@
+/*
+ * Gemelo - A person for chatting with strangers in a foreign language
+ * Copyright (C) 2011  Neil Roberts
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <glib-object.h>
+#include <string.h>
+
+#include "gml-person.h"
+
+G_DEFINE_TYPE (GmlPerson, gml_person, G_TYPE_OBJECT);
+
+static void
+gml_person_class_init (GmlPersonClass *klass)
+{
+}
+
+static void
+gml_person_init (GmlPerson *self)
+{
+}
+
+gboolean
+gml_person_id_equal (gconstpointer v1,
+                     gconstpointer v2)
+{
+  return *(const GmlPersonId *) v1 == *(const GmlPersonId *) v2;
+}
+
+guint
+gml_person_id_hash (gconstpointer v)
+{
+  G_STATIC_ASSERT (sizeof (GmlPersonId) == sizeof (gint64));
+
+  return g_int64_hash (v);
+}
+
+GmlPersonId
+gml_person_generate_id (GSocketAddress *address)
+{
+  GmlPersonId id = 0;
+  int i;
+
+  /* Generate enough random numbers to fill the id */
+  for (i = 0; i < sizeof (id) / sizeof (guint32); i++)
+    id |= (GmlPersonId) g_random_int () << (i * sizeof (guint32) * 8);
+
+  if (address)
+    {
+      gssize native_size = g_socket_address_get_native_size (address);
+      guint8 address_buf[native_size];
+
+      /* XOR the bytes of the connection address so that even if
+         someone can work out the sequence of random numbers it's
+         still hard to predict what the next id will be */
+
+      if (g_socket_address_to_native (address,
+                                      address_buf,
+                                      native_size,
+                                      NULL /* error */))
+        {
+          int address_pos = 0;
+          guint8 *p = (guint8 *) &id;
+
+          for (i = 0; i < sizeof (id); i++)
+            {
+              p[i] ^= address_buf[address_pos];
+              if (++address_pos >= native_size)
+                address_pos = 0;
+            }
+        }
+      else
+        g_warning ("g_socket_address_to_native failed");
+    }
+
+  return id;
+}
+
+GmlPerson *
+gml_person_new (GmlPersonId id)
+{
+  GmlPerson *person = g_object_new (GML_TYPE_PERSON, NULL);
+
+  person->id = id;
+
+  return person;
+}
---
+++ src/gml-person.h
@@ -0,0 +1,83 @@
+/*
+ * Gemelo - A person for chatting with strangers in a foreign language
+ * Copyright (C) 2011  Neil Roberts
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GML_PERSON_H__
+#define __GML_PERSON_H__
+
+#include <glib-object.h>
+#include <gio/gio.h>
+
+G_BEGIN_DECLS
+
+#define GML_TYPE_PERSON                                                 \
+  (gml_person_get_type())
+#define GML_PERSON(obj)                                                 \
+  (G_TYPE_CHECK_INSTANCE_CAST ((obj),                                   \
+                               GML_TYPE_PERSON,                         \
+                               GmlPerson))
+#define GML_PERSON_CLASS(klass)                                         \
+  (G_TYPE_CHECK_CLASS_CAST ((klass),                                    \
+                            GML_TYPE_PERSON,                            \
+                            GmlPersonClass))
+#define GML_IS_PERSON(obj)                                              \
+  (G_TYPE_CHECK_INSTANCE_TYPE ((obj),                                   \
+                               GML_TYPE_PERSON))
+#define GML_IS_PERSON_CLASS(klass)                                      \
+  (G_TYPE_CHECK_CLASS_TYPE ((klass),                                    \
+                            GML_TYPE_PERSON))
+#define GML_PERSON_GET_CLASS(obj)                                       \
+  (G_TYPE_INSTANCE_GET_CLASS ((obj),                                    \
+                              GML_PERSON,                               \
+                              GmlPersonClass))
+
+typedef guint64 GmlPersonId;
+
+typedef struct _GmlPerson      GmlPerson;
+typedef struct _GmlPersonClass GmlPersonClass;
+
+struct _GmlPersonClass
+{
+  GObjectClass parent_class;
+};
+
+struct _GmlPerson
+{
+  GObject parent;
+
+  GmlPersonId id;
+};
+
+GType
+gml_person_get_type (void) G_GNUC_CONST;
+
+gboolean
+gml_person_id_equal (gconstpointer v1,
+                     gconstpointer v2);
+
+guint
+gml_person_id_hash (gconstpointer v);
+
+GmlPersonId
+gml_person_generate_id (GSocketAddress *address);
+
+GmlPerson *
+gml_person_new (GmlPersonId id);
+
+G_END_DECLS
+
+#endif /* __GML_PERSON_H__ */
