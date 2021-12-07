4e25a070c99680c87e701d18f42ebffb55bb730d
Neil Roberts <bpeeluk@yahoo.co.uk>
2021-12-07 12:48:26 +0100

    Remove HAVE_CONFIG_H
    Instead it now just includes config.h unconditionally in all C files.

--- common/vsx-list.c
+++ common/vsx-list.c
@@ -23,9 +23,7 @@
 
 /* This list implementation is based on the Wayland source code */
 
-#ifdef HAVE_CONFIG_H
 #include "config.h"
-#endif
 
 #include <stdlib.h>
 #include <string.h>
--- server/vsx-key-value.c
+++ server/vsx-key-value.c
@@ -21,9 +21,7 @@
  * OF THIS SOFTWARE.
  */
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
+#include "config.h"
 
 #include "vsx-key-value.h"
 
