50b63ddc2f7dce63b741a2cfff99b0da8eba678a
Neil Roberts <bpeeluk@yahoo.co.uk>
2021-12-08 13:51:26 +0100

    client: Add a utility to get the monotonic clock

---
+++ client/vsx-monotonic.c
@@ -0,0 +1,33 @@
+/*
+ * Verda Ŝtelo - An anagram game in Esperanto for the web
+ * Copyright (C) 2021  Neil Roberts
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include "vsx-monotonic.h"
+
+#include <time.h>
+
+int64_t
+vsx_monotonic_get(void)
+{
+        struct timespec ts;
+
+        clock_gettime(CLOCK_MONOTONIC, &ts);
+
+        return ts.tv_sec * UINT64_C(1000000) + ts.tv_nsec / UINT64_C(1000);
+}
