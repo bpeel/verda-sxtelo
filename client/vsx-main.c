4b7983de6011f46711993e27d1fcc7875854895b
Neil Roberts <bpeeluk@yahoo.co.uk>
2021-12-23 10:50:53 +0100

    client/main: Queue a redraw when game_state modified signal received

--- client/vsx-main.c
+++ client/vsx-main.c
@@ -70,8 +70,10 @@ struct vsx_main_data {
 
         struct vsx_connection *connection;
         struct vsx_worker *worker;
-        struct vsx_game_state *game_state;
         struct vsx_asset_manager *asset_manager;
+
+        struct vsx_game_state *game_state;
+        struct vsx_listener modified_listener;
 
         bool button_pressed;
         int mouse_x, mouse_y;
@@ -789,6 +791,31 @@ init_sdl(struct vsx_main_data *main_data)
 }
 
 static void
+game_state_modified_cb(struct vsx_listener *listener,
+                       void *user_data)
+{
+        struct vsx_main_data *main_data =
+                vsx_container_of(listener,
+                                 struct vsx_main_data,
+                                 modified_listener);
+
+        queue_redraw_unlocked(main_data);
+}
+
+static void
+init_game_state(struct vsx_main_data *main_data)
+{
+        main_data->game_state = vsx_game_state_new(main_data->worker,
+                                                   main_data->connection);
+
+        struct vsx_signal *modified_signal =
+                vsx_game_state_get_modified_signal(main_data->game_state);
+
+        main_data->modified_listener.notify = game_state_modified_cb;
+        vsx_signal_add(modified_signal, &main_data->modified_listener);
+}
+
+static void
 redraw_needed_cb(struct vsx_listener *listener,
                  void *signal_data)
 {
@@ -849,8 +876,7 @@ main(int argc, char **argv)
                 goto out;
         }
 
-        main_data->game_state = vsx_game_state_new(main_data->worker,
-                                                   main_data->connection);
+        init_game_state(main_data);
 
         if (!init_painter(main_data)) {
                 ret = EXIT_FAILURE;
