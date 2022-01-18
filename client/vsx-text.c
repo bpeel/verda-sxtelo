6560706b9f04663304fc6eb171c3de4dd14c5557
Neil Roberts <bpeeluk@yahoo.co.uk>
2022-01-18 17:10:31 +0100

    client: Add a module to contain the translatable text

---
+++ client/vsx-text.c
@@ -0,0 +1,65 @@
+/*
+ * Verda Ŝtelo - An anagram game in Esperanto for the web
+ * Copyright (C) 2022  Neil Roberts
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include "vsx-text.h"
+
+#include <assert.h>
+
+#include "vsx-util.h"
+
+static const char *const
+english[] = {
+        [VSX_TEXT_LANGUAGE_CODE] = "en",
+};
+
+static const char *const
+french[] = {
+        [VSX_TEXT_LANGUAGE_CODE] = "fr",
+};
+
+static const char *const
+esperanto[] = {
+        [VSX_TEXT_LANGUAGE_CODE] = "eo",
+};
+
+static const char *const * const
+languages[] = {
+        [VSX_TEXT_LANGUAGE_ENGLISH] = english,
+        [VSX_TEXT_LANGUAGE_FRENCH] = french,
+        [VSX_TEXT_LANGUAGE_ESPERANTO] = esperanto,
+};
+
+_Static_assert(VSX_N_ELEMENTS(languages) == VSX_TEXT_N_LANGUAGES,
+               "The n_languages enum needs to match the number of languagues "
+               "in the array");
+_Static_assert(VSX_N_ELEMENTS(english) == VSX_N_ELEMENTS(french),
+               "Every string needs to be defined for every language");
+_Static_assert(VSX_N_ELEMENTS(english) == VSX_N_ELEMENTS(esperanto),
+               "Every string needs to be defined for every language");
+
+const char *
+vsx_text_get(enum vsx_text_language language,
+             enum vsx_text text)
+{
+        assert(language < VSX_TEXT_N_LANGUAGES);
+        assert(text < VSX_N_ELEMENTS(english));
+
+        return languages[language][text];
+}
---
+++ client/vsx-text.h
@@ -0,0 +1,38 @@
+/*
+ * Verda Ŝtelo - An anagram game in Esperanto for the web
+ * Copyright (C) 2022  Neil Roberts
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef VSX_TEXT_H
+#define VSX_TEXT_H
+
+enum vsx_text_language {
+        VSX_TEXT_LANGUAGE_ENGLISH,
+        VSX_TEXT_LANGUAGE_FRENCH,
+        VSX_TEXT_LANGUAGE_ESPERANTO,
+};
+
+enum vsx_text {
+        VSX_TEXT_LANGUAGE_CODE,
+};
+
+#define VSX_TEXT_N_LANGUAGES 3
+
+const char *
+vsx_text_get(enum vsx_text_language language,
+             enum vsx_text text);
+
+#endif /* VSX_TEXT_H */
