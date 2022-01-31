504d0f6884de73e7809b327a687327fba81e30ca
Neil Roberts <bpeeluk@yahoo.co.uk>
2022-01-31 10:32:31 +0100

    Add a script to get the freetype dependency
    Instead of just assuming there is some freetype source code in a
    freetype directory in the root of the source tree, there is now a script
    to download a specific tarball. The tarball is checked against a known
    sha256 hash. That way we can have the exact freetype version information
    checked into the source tree.

--- app/build-freetype-ninja.sh
+++ app/build-freetype-ninja.sh
@@ -6,6 +6,15 @@ rm -f "$1"
 
 src_dir=$(dirname "$0")
 
+source "$src_dir/../freetype-version.sh"
+FREETYPE_DIR="${src_dir}/../freetype-$FREETYPE_VERSION"
+
+if ! test -d "$FREETYPE_DIR"; then
+    echo "The freetype sources are missing from $FREETYPE_DIR" >&2
+    echo "Please run the get-dependencies.sh script before building" >&2
+    exit 1
+fi
+
 source "$src_dir/get_build_type.sh"
 
 meson -Dbrotli=disabled \
@@ -17,7 +26,7 @@ meson -Dbrotli=disabled \
       -Dprefix=$(pwd)/freetype-install \
       -Dbuildtype="$build_type" \
       freetype-build \
-      "$src_dir"/../freetype \
+      "$FREETYPE_DIR" \
       --cross-file=cross.txt
 
 ninja -C freetype-build
